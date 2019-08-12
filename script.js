e928c4d797e9307d65ef75591022477503db1a41
guinuxbr <glmarques@crmes.local>
2019-08-12 18:34:28 -0300

    Evoluindo o CSS da página.

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function calcular() {
-    var pesoNovelo = document.getElementById('pesoNovelo').value
-    var valorNovelo = document.getElementById("valorNovelo").value;
-    var pesoFio = document.getElementById("pesoFio").value;
-    var valorHora = document.getElementById("valorHora").value;
-    var horasTrabalhadas = document.getElementById("horasTrabalhadas").value;
-    var resultado = document.getElementById('resultado');
-
+    let pesoNovelo = document.getElementById('pesoNovelo').value
+    let valorNovelo = document.getElementById("valorNovelo").value;
+    let pesoFio = document.getElementById("pesoFio").value;
+    let valorHora = document.getElementById("valorHora").value;
+    let horasTrabalhadas = document.getElementById("horasTrabalhadas").value;
+    let resultado = document.getElementById('resultado');
+    
     if (pesoNovelo < 1 || valorNovelo < 1 || pesoFio < 1 || valorHora < 1 || horasTrabalhadas < 1) {
         resultado.innerHTML = `<font color="red">Os campos devem ter o valor de, no mínimo 1(um)!!!</font>`
         window.alert(`Os campos devem ter o valor de, no mínimo 1(um)!!!`)
@@ -15,15 +15,22 @@ function calcular() {
         window.alert(`Você deve preencher todos os campos!!!`)
 
     } else {
-        var valorGrama = valorNovelo / pesoNovelo;
-        var valorLinha = valorGrama * pesoFio;
-        var totalTrabalhado = valorHora * horasTrabalhadas;
-        var valorDaPeca = valorLinha + totalTrabalhado
+        let valorGrama = valorNovelo / pesoNovelo;
+        let valorLinha = valorGrama * pesoFio;
+        let totalTrabalhado = valorHora * horasTrabalhadas;
+        let valorDaPeca = valorLinha + totalTrabalhado
 
         resultado.innerHTML = `O valor do fio utilizado foi ${valorLinha.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}.<br>`
         resultado.innerHTML += `O valor das horas trabalhadas foi ${totalTrabalhado.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}.<br>`
         resultado.innerHTML += `Você deve cobrar aproximadamente ${valorDaPeca.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}!`
     }
+}
 
-    
+function limpar() {
+    document.getElementById('pesoNovelo').value = ""
+    document.getElementById("valorNovelo").value = ""
+    document.getElementById("pesoFio").value = ""
+    document.getElementById("valorHora").value = ""
+    document.getElementById("horasTrabalhadas").value = ""
+    resultado.innerHTML = `Preencha os dados acima para ver o resultado!`
 }
